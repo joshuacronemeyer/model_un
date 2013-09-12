398a771d56e7edb6a6bc2df96bdd99ec39e199d8
Josh Cronemeyer <joshuacronemeyer@gmail.com>
2013-09-12 15:24:50 -0700

    Simplified to not care about the difference between states, provinces, and countries.  Add canadain provinces.

--- lib/model_un.rb
+++ lib/model_un.rb
@@ -5,23 +5,20 @@ module ModelUN
   class << self
     
     def convert(string)
-      if string.length == 2
+      if string.length <= 3
         self.convert_state_abbr(string.upcase)
-      elsif string.length == 3
-        self.convert_nato_country_abbr(string.upcase)
       else
-        self.convert_state_or_country_name(string.upcase)
+        self.convert_state_name(string.upcase)
       end
     end
     
-    # US STATES
     def convert_state_abbr(state_abbr)
       self.convert_state_abbreviation(state_abbr)
     end
     
     def convert_state_abbreviation(state_abbr)
       begin
-        return US_STATE_NAMES[US_STATE_ABBR.index(state_abbr)].downcase.gsub(/\b\w/){$&.upcase}
+        return STATE_NAMES[STATE_ABBR.index(state_abbr)].downcase.gsub(/\b\w/){$&.upcase}
       rescue Exception
         return state_abbr
       end
@@ -29,49 +26,10 @@ module ModelUN
     
     def convert_state_name(state_name)
       begin
-        return US_STATE_ABBR[US_STATE_NAMES.index(state_name.upcase)]
+        return STATE_ABBR[STATE_NAMES.index(state_name.upcase)]
       rescue Exception
         return state_name.downcase.gsub(/\b\w/){$&.upcase}
       end
     end
-    
-    # COUNTRIES (NATO CODES)
-    def convert_country_abbr(country_abbr)
-      self.convert_country_abbr(country_abbr)
-    end
-    
-    def convert_nato_country_abbr(country_abbr)
-      begin
-        return NATO_COUNTRY_NAMES[NATO_ABBR.index(country_abbr)].downcase.gsub(/\b\w/){$&.upcase}
-      rescue Exception
-        return country_abbr
-      end
-    end
-    
-    def convert_country_name(country_name)
-      self.convert_nato_country_name(country_name)
-    end
-    
-    def convert_nato_country_name(country_name)
-      begin
-        return NATO_ABBR[NATO_COUNTRY_NAMES.index(country_name.upcase)]
-      rescue Exception
-        return country_name.downcase.gsub(/\b\w/){$&.upcase}
-      end
-    end
-    
-    def convert_state_or_country_name(name)
-      state_abbr = self.convert_state_name(name)
-      if state_abbr.upcase == name
-        return self.convert_nato_country_name(name)
-      else
-        return state_abbr
-      end
-    end
-    
-    def export_states_list
-      {'US States' => US_STATES}.to_yaml.gsub(/\n/,'').gsub(/-/, '').strip
-    end
-    
   end
 end
--- spec/model_un_spec.rb
+++ spec/model_un_spec.rb
@@ -3,8 +3,8 @@ require 'model_un'
 describe ModelUN do
   
   it "should be able to read the data hash" do
-    ModelUN::US_STATE_NAMES.should_not == nil
-    ModelUN::US_STATE_ABBR.should_not == nil
+    ModelUN::STATE_NAMES.should_not == nil
+    ModelUN::STATE_ABBR.should_not == nil
   end
   
   describe "states" do
@@ -17,7 +17,6 @@ describe ModelUN do
     it "should return the input if no match on full name" do
       state = 'Foobar'
       ModelUN.convert(state).should == state
-      
     end
     
     it "should return a full state for an abbreviation" do
@@ -37,38 +36,23 @@ describe ModelUN do
       ModelUN.convert(state).should == 'NH'
       ModelUN.convert_state_name(state).should == 'NH'
     end
-  end
 
-
-  describe "countries" do
-    it "should return the input if no match on country abbreviation" do
-      abbr = 'XX'
-      ModelUN.convert(abbr).should == 'XX'
-      ModelUN.convert_nato_country_abbr(abbr).should == 'XX'
-    end
-
-    it "should return the input if no match on country full name" do
-      country = 'Foobar'
-      ModelUN.convert(country).should == 'Foobar'
-      ModelUN.convert_nato_country_name(country).should == 'Foobar'
-    end
-    
     it "should return a full country name for an abbreviation" do
       abbr = 'SWE'
       ModelUN.convert(abbr).should == 'Sweden'
-      ModelUN.convert_nato_country_abbr(abbr).should == 'Sweden'
+      ModelUN.convert_state_abbr(abbr).should == 'Sweden'
     end
     
     it "should return an abbreviation for a full country name" do
       country = 'Sweden'
       ModelUN.convert(country).should == 'SWE'
-      ModelUN.convert_nato_country_name(country).should == 'SWE'
+      ModelUN.convert_state_name(country).should == 'SWE'
     end
     
     it "should return an abbreviation full country name for a country name with two words" do
       country = 'Czech Republic'
       ModelUN.convert(country).should == 'CZE'
-      ModelUN.convert_nato_country_name(country).should == 'CZE'
+      ModelUN.convert_state_name(country).should == 'CZE'
     end
   end
 
